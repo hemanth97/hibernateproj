ca2282c9e49e2d6e9d8840e99a42ee51c1500a04
Hemanth.T <hemanth97612@gmail.com>
2018-03-03 19:56:03 +0530

    hibernatedemo

---
+++ src/main/java/model/ProductDAO.java
@@ -0,0 +1,79 @@
+package model;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+public class ProductDAO {
+	private Transaction trans;
+	private Session sess;
+	private boolean b = true;
+	public boolean insertProduct(Product p)
+	{
+		try
+		{
+		DbConfig db = new DbConfig();
+		 sess = db.getSess();
+		 trans = sess.beginTransaction();
+		// Product p1 =(Product)sess.get(Product.class, p.getPid());
+		//sess.save(p1);
+		sess.save(p);
+		trans.commit();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+			trans.rollback();
+			b = false;
+		}
+		return b;
+	}
+	public List<Product> getProducts()
+	{
+		Query query = sess.createQuery("from Product");
+		List<Product> lp = (List<Product>)query.getResultList();
+		
+		return lp;
+		
+	}
+	public boolean updateProduct(Product p)
+	{
+		try
+		{
+		DbConfig db = new DbConfig();
+		 sess = db.getSess();
+		 trans = sess.beginTransaction();
+		// Product p1 =(Product)sess.get(Product.class, p.getPid());
+		//sess.update(p1);
+		sess.update(p);
+		trans.commit();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+			trans.rollback();
+			b = false;
+		}
+		return b;
+	}
+	public boolean deleteProduct(Product p)
+	{
+		try
+		{
+		DbConfig db = new DbConfig();
+		 sess = db.getSess();
+		 trans = sess.beginTransaction();
+		 Product p1 =(Product)sess.get(Product.class, p.getPid());
+		 sess.delete(p1);
+		
+		trans.commit();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+			trans.rollback();
+			b = false;
+		}
+		return b;
+	}
+
+}
